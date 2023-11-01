b23ed702d29c61ba3694efc5c49c88441e58bf39
youngdgg <144021000+youngdgg@users.noreply.github.com>
2023-11-01 09:31:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+document.getElementById("title").style.color = "red";
+function myFunction() {
+    document.getElementById("myH2").style.color = "#ff0000";
+    document.getElementById("myP").style.color = "magenta";
+    document.getElementById("myP2").style.color = "blue";
+    document.getElementById("myDiv").style.color = "lightblue";
+  }
---
+++ app6.1.js
@@ -0,0 +1,7 @@
+function kleuraanpassen()
+{
+document.getElementById("kop1").style.color = "blue";
+document.getElementById("kop1").innerHTML = "i have changed!";
+}
+
+kleuraanpassen();
